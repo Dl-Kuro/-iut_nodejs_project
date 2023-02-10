243d765077cef42594a9d84e8216379d989d9722
Kuro <kw779966@gmail.com>
2023-02-10 16:49:04 +0100

    Add movie services

--- server/manifest.js
+++ server/manifest.js
@@ -50,7 +50,7 @@ module.exports = new Confidence.Store({
                                 host: process.env.DB_HOST || '0.0.0.0',
                                 user: process.env.DB_USER || 'root',
                                 password: process.env.DB_PASSWORD || 'hapi',
-                                database: process.env.DB_DATABaASE || 'user'
+                                database: process.env.DB_DATABASE || 'user'
                             }
                         }
                     },
